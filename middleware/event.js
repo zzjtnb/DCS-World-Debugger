dbfc721aa0fcf5ac15445882d23a304f56166da7
zzjtnb <zhengzhujituan@gmail.com>
2020-11-24 15:22:15 +0800

    update

--- middleware/event.js
+++ middleware/event.js
@@ -1,7 +1,7 @@
 const { EventEmitter } = require('events');
 const emitter = new EventEmitter()
 emitter.on('error', (err) => {
-  console.log(err)
+  // console.log(err)
 })
 emitter.setMaxListeners(0)
 module.exports = emitter
