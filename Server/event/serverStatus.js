3fc61baf04c987b8d6f5fd78aa6d2754458fbbe2
争逐 <zhengzhujituan@gmail.com>
2021-03-24 22:22:10 +0800

    Delete Server directory

--- Server/event/serverStatus.js
+++
@@ -1,5 +0,0 @@
-const event = require('../../middleware/event');
-const { serverStatus } = require('../../middleware/logger');
-event.on('serverStatus', (msg) => {
-  serverStatus.info(msg.data.msg);
-});
